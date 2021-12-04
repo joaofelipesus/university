6d523ac08cadef087424021a39192451191bdb70
joaofelipe <joaofelipe1294@gmail.com>
2021-12-04 10:29:36 -0300

    feat: add model teacher.

---
+++ lib/university/teacher.ex
@@ -0,0 +1,20 @@
+defmodule University.Teacher do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @required_params [:name, :graduation]
+
+  schema "teachers" do
+    field :name, :string
+    field :graduation, :string
+    timestamps()
+  end
+
+  def changeset(enum \\ %__MODULE__{}, params) do
+    enum
+    |> cast(params, @required_params)
+    |> validate_required(@required_params)
+    |> validate_inclusion(:graduation, ["POST_GRADUATED", "MASTER", "DOCTOR", "POST_DOCTOR"])
+  end
+end
