b20d07a443e0086aa86f5260b0e5ceafb01bbc4e
joaofelipe <joaofelipe1294@gmail.com>
2021-12-05 07:28:02 -0300

    feat: add route show.

--- lib/university_web/controllers/teachers_controller.ex
+++ lib/university_web/controllers/teachers_controller.ex
@@ -2,10 +2,19 @@ defmodule UniversityWeb.TeachersController do
   use UniversityWeb, :controller
 
   alias University.{Repo, Teacher}
+  alias University.Services.Teachers.Find
 
   def index(conn, _params) do
     conn
     |> put_status(:ok)
     |> render("index.json", %{teachers: Repo.all(Teacher)})
+  end
+
+  def show(conn, %{"id" => id}) do
+    with {:ok, teacher: %Teacher{} = teacher} <- Find.by_id(id) do
+      conn
+      |> put_status(:ok)
+      |> render("show.json", %{teacher: teacher})
+    end
   end
 end
--- test/university_web/views/teachers_view_test.exs
+++ test/university_web/views/teachers_view_test.exs
@@ -1,14 +1,14 @@
 defmodule UniversityWeb.TeachersViewTest do
   use UniversityWeb.ConnCase, async: true
   import Phoenix.View
+  import University.Factory
+
 
   alias University.{Teacher, Repo}
 
   describe "render('index.json')/2" do
     test "return a list with all teachers" do
-      %{name: "Some Teacher", graduation: "MASTER"}
-      |> Teacher.changeset
-      |> Repo.insert
+      insert(:teacher)
 
       teachers = Repo.all(Teacher)
 
@@ -16,6 +16,14 @@ defmodule UniversityWeb.TeachersViewTest do
       %{teachers: serialized_teachers} = response
 
       assert serialized_teachers == teachers
+    end
+  end
+
+  describe "render('show.json', %{teacher: teacher})" do
+    test "return a teacher serialized" do
+      teacher = insert(:teacher)
+      %{teacher: serialized_teacher} = render(UniversityWeb.TeachersView, "show.json", %{teacher: teacher})
+      assert teacher == serialized_teacher
     end
   end
 end
