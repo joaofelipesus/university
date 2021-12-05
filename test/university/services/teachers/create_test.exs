13c24f93cf74f39f7d7938793c5e07b9c9d3ebc2
joaofelipe <joaofelipe1294@gmail.com>
2021-12-05 16:49:28 -0300

    test: add create test.

---
+++ test/university/services/teachers/create_test.exs
@@ -0,0 +1,29 @@
+defmodule University.Services.Teachers.CreateTest do
+  use University.DataCase, async: true
+
+  alias University.Services.Teachers.Create
+  alias University.Teacher
+
+  describe "call/1" do
+    test "when data is ok" do
+      params = %{name: "Asuka", graduation: "MASTER"}
+      response = Create.call(params)
+      assert {:ok, %Teacher{}} = response
+    end
+
+    test "when param has problems" do
+      params = %{name: "", graduation: "SUPREME_MASTER"}
+      response = Create.call(params)
+      assert {:error, %Ecto.Changeset{} = changeset} = response
+      %{errors: errors} = changeset
+      assert errors ==[
+        graduation: {"is invalid",
+         [
+           validation: :inclusion,
+           enum: ["POST_GRADUATED", "MASTER", "DOCTOR", "POST_DOCTOR"]
+         ]},
+        name: {"can't be blank", [validation: :required]}
+      ]
+    end
+  end
+end
